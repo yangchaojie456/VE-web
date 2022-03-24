2338fa1dacebd928b716601340ae371c7964baed
Your Name <you@example.com>
2022-03-24 12:25:54 +0800

    格式化测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>
+        <span>格式化测试</span>
+      </p>
     </div>
   );
 }
